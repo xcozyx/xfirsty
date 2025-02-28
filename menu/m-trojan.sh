9ad61be967074ca0c852409588e42cd03330f85d
xcozy <ferizacky@gmail.com>
2025-03-01 00:05:47 +0700

    sc
    sc

--- menu/m-trojan.sh
+++ menu/m-trojan.sh
@@ -749,7 +749,7 @@ fi
 function cek-tr(){
 clear
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '^#!' | cut -d ' ' -f 2 | sort | uniq`);
+data=( `cat /etc/xray/config.json | grep '^#tr' | cut -d ' ' -f 2 | sort | uniq`);
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
 echo -e "$COLOR1│${NC}${COLBG1}             ${WH}• TROJAN USER ONLINE •               ${NC}$COLOR1│ $NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
